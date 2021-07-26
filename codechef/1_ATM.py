896d508c60796ce997d7268fc16eeaf0a5d93cdd
hungryc0d3r <71399975+hungryc0d3r@users.noreply.github.com>
2021-07-26 06:35:41 -0700

    codechef

--- codechef/1_ATM.py
+++ codechef/1_ATM.py
@@ -1,9 +1,18 @@
 '''Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction 
 if X is a multiple of 5, and Pooja's account balance has enough cash to perform the withdrawal transaction 
 (including bank charges). For each successful withdrawal the bank charges 0.50 $US. Calculate Pooja's 
-account balance after an attempted transaction.'''
+account balance after an attempted transaction.
 
-# input -> withdrawal amt and intial amt balance
+
+Input
+Positive integer 0 < X <= 2000 - the amount of cash which Pooja wishes to withdraw.
+
+Nonnegative number 0<= Y <= 2000 with two digits of precision - Pooja's initial account balance.
+
+Output
+Output the account balance after the attempted transaction, given as a number with two digits of precision. 
+If there is not enough money in the account to complete the transaction, output the current bank balance.'''
+
 
 withdrawal = int(input('Enter withdraw amt: '))
 initial = float(input('Enter initial amt: '))
