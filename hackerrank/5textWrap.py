fd33a9f9d1ed5942305c78267afdadf835296805
hungryc0d3r <71399975+hungryc0d3r@users.noreply.github.com>
2022-02-12 11:31:14 +0530

    Create 5textWrap.py

---
+++ hackerrank/5textWrap.py
@@ -0,0 +1,14 @@
+"""
+given a string s and width w.
+task is to wrap the string into a paragraph of width w.
+"""
+import textwrap
+
+def wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width=max_width)
+    return wrapper.fill(text=string)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
